07dc86d77f1c801590d3a50e8970c3f98ab5cbcd
RAPHAEL REZENDE DOS SANTOS ROCHA <121191119+rocha-raphael@users.noreply.github.com>
2024-02-29 16:24:05 -0300

    summary

--- static/javascript/ponto.js
+++ static/javascript/ponto.js
@@ -24,3 +24,62 @@ function atualizarRelogio() {
     }
 }
 
+function obterLocalizacao() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(enviarLocalizacao, mostrarErro);
+  } else { 
+    alert("Geolocalização não é suportada por este navegador.");
+  }
+}
+
+function enviarLocalizacao(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  // Aqui você pode enviar os dados para o servidor Django
+  fetch('/caminho-para-sua-view/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      // Inclua isso se estiver usando CSRF tokens em suas forms Django
+      'X-CSRFToken': getCookie('csrftoken'), 
+    },
+    body: JSON.stringify({latitude: latitude, longitude: longitude})
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch((error) => console.error('Erro:', error));
+}
+
+function mostrarErro(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      alert("Usuário negou a solicitação de Geolocalização.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert("Informação de localização indisponível.");
+      break;
+    case error.TIMEOUT:
+      alert("A solicitação para obter a localização do usuário expirou.");
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("Ocorreu um erro desconhecido.");
+      break;
+  }
+}
+
+// Função para obter o valor do cookie, útil para CSRFToken
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
