540a7f8d48bd6efe72045a972f6d801b6fcb4065
RAPHAEL REZENDE DOS SANTOS ROCHA <121191119+rocha-raphael@users.noreply.github.com>
2024-02-29 17:16:04 -0300

    teste

--- static/javascript/ponto.js
+++ static/javascript/ponto.js
@@ -11,10 +11,7 @@ function baterPonto() {
         clearInterval(intervalId); // Para o relógio
         var horarioAtual = new Date().toLocaleTimeString();
         document.getElementById('clock').style.display = 'none'; // Oculta o relógio
-        document.getElementById('mensagem').innerText = `Ponto registrado às ${horarioAtual}. Tenha um bom dia ${nome_usuario}!`;
-    } else {
-        document.getElementById('mensagem').innerText = `O relógio já foi parado e o ponto registrado. Recarregue a página para reiniciar.`;
-    }
+    } 
 }
 
 function atualizarRelogio() {
@@ -25,55 +22,3 @@ function atualizarRelogio() {
 }
 
 
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else { 
-        document.getElementById("location").innerHTML = "Geolocalização não é suportada por este navegador.";
-    }
-}
-
-function showPosition(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    document.getElementById("location").innerHTML = "Latitude: " + latitude + 
-    "<br>Longitude: " + longitude;
-}
-
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            document.getElementById("location").innerHTML = "Usuário negou a solicitação de Geolocalização."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            document.getElementById("location").innerHTML = "Informação de localização indisponível."
-            break;
-        case error.TIMEOUT:
-            document.getElementById("location").innerHTML = "A solicitação para obter a localização do usuário expirou."
-            break;
-        case error.UNKNOWN_ERROR:
-            document.getElementById("location").innerHTML = "Ocorreu um erro desconhecido."
-            break;
-    }
-}
-function showPosition(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    
-    // Aqui você pode enviar a latitude e longitude para o servidor
-    $.ajax({
-        url: '/index/ponto/',
-        type: 'POST',
-        data: {
-            'latitude': latitude,
-            'longitude': longitude
-        },
-        success: function(response) {
-            console.log(response);
-        },
-        error: function(error) {
-            console.error(error);
-        }
-    });
-}
