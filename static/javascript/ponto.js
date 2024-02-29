698860a00ba1e1eacbe2cee111882f0022735425
RAPHAEL REZENDE DOS SANTOS ROCHA <121191119+rocha-raphael@users.noreply.github.com>
2024-02-29 09:43:05 -0300

    Update ponto.js

--- static/javascript/ponto.js
+++ static/javascript/ponto.js
@@ -1,7 +1,5 @@
-
 var relógioAtivo = true;
 var intervalId;
-
 window.onload = function() {
     document.getElementById('nome_usuario').value = nome_usuario;
     intervalId = setInterval(atualizarRelogio, 1000);
@@ -12,10 +10,10 @@ function baterPonto() {
         relógioAtivo = false;
         clearInterval(intervalId); // Para o relógio
         var horarioAtual = new Date().toLocaleTimeString();
+        document.getElementById('clock').style.display = 'none'; // Oculta o relógio
         document.getElementById('mensagem').innerText = `Ponto registrado às ${horarioAtual}. Tenha um bom dia, ${nome_usuario}!`;
     } else {
         document.getElementById('mensagem').innerText = `O relógio já foi parado e o ponto registrado. Recarregue a página para reiniciar.`;
-
     }
 }
 
