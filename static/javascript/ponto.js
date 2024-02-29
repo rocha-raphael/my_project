b731ef6ae44836c439d78803703856dc95c98560
RAPHAEL REZENDE DOS SANTOS ROCHA <121191119+rocha-raphael@users.noreply.github.com>
2024-02-29 09:27:19 -0300

    clcok

--- static/javascript/ponto.js
+++ static/javascript/ponto.js
@@ -12,11 +12,10 @@ function baterPonto() {
         relógioAtivo = false;
         clearInterval(intervalId); // Para o relógio
         var horarioAtual = new Date().toLocaleTimeString();
-        document.getElementById('mensagem').innerText = `Ponto registrado às ${horarioAtual}. Tenha um bom dia, ${nome_usuario}!`;
-        document.getElementById('clock').innerText = `teste`;
+        document.getElementById('clock').innerText = `Ponto registrado às ${horarioAtual}. Tenha um bom dia, ${nome_usuario}!`;
     } else {
-        document.getElementById('mensagem').innerText = `O relógio já foi parado e o ponto registrado. Recarregue a página para reiniciar.`;
-        document.getElementById('clock').innerText = `teste`;
+        document.getElementById('clock').innerText = `O relógio já foi parado e o ponto registrado. Recarregue a página para reiniciar.`;
+
     }
 }
 
@@ -24,7 +23,5 @@ function atualizarRelogio() {
     if (relógioAtivo) {
         var horarioAtualizado = new Date().toLocaleTimeString();
         document.getElementById('clock').innerText = `Olá, ${nome_usuario}! Horário atual: ${horarioAtualizado}`;
-    } else {
-        document.getElementById('clock').innerText = `teste`;
     }
 }
